ab8e727161775e096358bb7fa878458044b56f10
ohtwo <hello@tinygram.dev>
2024-01-04 17:15:17 +0900

    feat: ch2 - add Effect

--- TryTCA/CounterFeature.swift
+++ TryTCA/CounterFeature.swift
@@ -13,11 +13,22 @@ import SwiftUI
 struct CounterFeature {
     struct State: Equatable {
         var count: Int = 0
+        var fact: String?
+        var isLoading: Bool = false
+        var isTimerRunning: Bool = false
     }
 
     enum Action {
         case decrementButtonTapped
         case incrementButtonTapped
+        case factButtonTapped
+        case factResponse(String)
+        case toggleTimerButtonTapped
+        case timerTick
+    }
+
+    enum CancelID {
+        case timer
     }
 
     var body: some ReducerOf<Self> {
@@ -25,10 +36,48 @@ struct CounterFeature {
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
                 return .none
 
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
+                return .none
+
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared
+                        .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact))
+                }
+
+            case let .factResponse(fact):
+                state.fact = fact
+                state.isLoading = false
+                return .none
+
+            case .toggleTimerButtonTapped:
+                state.isTimerRunning.toggle()
+
+                guard state.isTimerRunning else {
+                    return .cancel(id: CancelID.timer)
+                }
+
+                return .run { send in
+                    while true {
+                        try await Task.sleep(nanoseconds: 1_000_000_000)
+                        await send(.timerTick)
+                    }
+                }
+                .cancellable(id: CancelID.timer)
+
+            case .timerTick:
+                state.count += 1
+                state.fact = nil
                 return .none
             }
         }
@@ -62,6 +111,31 @@ struct CounterView: View {
                     .padding()
                     .background(Color.black.opacity(0.1))
                     .cornerRadius(10)
+                }
+                
+                Button(viewStore.isTimerRunning ? "Stop timer" : "Start timer") {
+                    viewStore.send(.toggleTimerButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(.black.opacity(0.1))
+                .cornerRadius(10)
+
+                Button("Fact") {
+                    viewStore.send(.factButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(.black.opacity(0.1))
+                .cornerRadius(10)
+
+                if viewStore.isLoading {
+                    ProgressView()
+                } else if let fact = viewStore.fact {
+                    Text(fact)
+                        .font(.largeTitle)
+                        .multilineTextAlignment(.center)
+                        .padding()
                 }
             }
         }
