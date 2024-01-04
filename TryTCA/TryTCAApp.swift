42c01f2cdbd1c1b73fe47a756e0c16e5e8309d15
ohtwo <hello@tinygram.dev>
2024-01-04 12:26:42 +0900

    feat: setup project

---
+++ TryTCA/TryTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryTCAApp.swift
+//  TryTCA
+//
+//  Created by Byeonghak Kang on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TryTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
